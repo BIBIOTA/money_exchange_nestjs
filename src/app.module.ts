4a46152297c531c94b582389eb3afb9f98a2ffda
BiBiOTA <yukiotataitien@gmail.com>
2021-12-15 00:03:48 +0800

    --fixed -- serveRoot -> api && rate.service currency model.findOne 執行敘述修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
+      serveRoot: '/api',
     }),
     CommandModule,
     ConfigModule.forRoot(),
